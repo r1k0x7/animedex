d8194e18aafa3cca0b5d7ef352c6d412b54b2103
r1k0x7 <yun798731@gmail.com>
2026-07-20 17:28:55 +0700

    Create use-debounce.ts

---
+++ src/hooks/use-debounce.ts
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+/** Tunda pembaruan nilai sampai tidak ada perubahan selama `delay` ms. */
+export function useDebounce<T>(value: T, delay = 400): T {
+  const [debounced, setDebounced] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebounced(value), delay);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debounced;
+}
