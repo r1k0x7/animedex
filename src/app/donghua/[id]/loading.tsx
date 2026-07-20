0093b510d0f077ca1b84237a0a1f23ae651d4a95
r1k0x7 <yun798731@gmail.com>
2026-07-20 17:23:53 +0700

    Add files via upload

---
+++ src/app/donghua/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import { DonghuaDetailSkeleton } from "@/components/skeletons";
+
+export default function Loading() {
+  return (
+    <div className="pb-8 pt-0">
+      <DonghuaDetailSkeleton />
+    </div>
+  );
+}
